fdc9c8a3d6e0a7539ed7044ba50ba03c85380884
Bambi på hal is <kronosjo.arvid@live.com>
2022-09-20 22:19:59 +0200

    remove dependencies from hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,11 +1,11 @@
-const ffmpeg = require('@ffmpeg-installer/ffmpeg');
-console.log(ffmpeg.path, ffmpeg.version);
+// const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+// console.log(ffmpeg.path, ffmpeg.version);
 
 function main(args) {
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
-    return {"body": ffmpeg.path}
+    return {"body": greeting}
   }
 
 exports.main = main

639bd578c549be0ab02067f7bd4c0a7bd138e39e
Bambi på hal is <kronosjo.arvid@live.com>
2022-09-20 21:11:14 +0200

    fix ffmpeg and fix autodeploy

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,8 +1,10 @@
+const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+
 function main(args) {
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
-    return {"body": greeting}
+    return {"body": ffmpeg.path}
   }
 
 exports.main = main
